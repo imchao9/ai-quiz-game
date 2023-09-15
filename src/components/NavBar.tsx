5604f9bdbad4dd3add42930bde91438a72d5b29f
Chao <dreamaker7@outlook.com>
2023-09-16 07:08:12 +0800

    Navbar login complete

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,6 +6,7 @@ import SignInButton from "./SignInButton";
 type Props = {};
 
 const NavBar = async (props: Props) => {
+    const session = await getAuthSession();
     return (
         <div className="fixed inset-x-0 top-0 bg-white dark:bg-gray-950 z-[10] h-fit border-b border-zinc-300 py-2">
             <div className="flex items-center justify-between h-full gap-2 px-8 mx-auto max-w-7xl">
@@ -15,10 +16,14 @@ const NavBar = async (props: Props) => {
                         Quiz App
                     </p>
                 </Link>
+                <div className="flex item-center">
+                    {session?.user ? (
+                        <h1>Welcome  {session.user.name}</h1>
 
-            </div>
-            <div className="flex item-center">
-                <SignInButton text={"Sign In"} />
+                    ) : (
+                        <SignInButton text={"Sign In"} />
+                    )}
+                </div>
             </div>
         </div>
     );
